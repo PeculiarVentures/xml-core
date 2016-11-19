c21a3eda95e659e47bd199fa45c3bc3e33e0d0db
microshine <microshine@mail.ru>
2016-11-19 14:54:30 +0300

    Update types

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,37 @@
-declare namespace xmljs {
+declare type SelectNodes = (node: Node, xpath: string) => Node[];
+
+interface IXmlSerializable {
+
+    Prefix: string;
+    /**
+     * Writes object to XML node
+     * @returns Node
+     */
+    GetXml(): Node;
+    /**
+     * Reads XML from string
+     * @param  {Node} node
+     * @returns void
+     */
+    LoadXml(node: Node): void;
+}
+
+interface AssocArray<T> {
+    [index: string]: T;
+}
+
+declare const select: SelectNodes;
+
+declare type XmlBufferEncoding = string | "utf8" | "binary" | "hex" | "base64" | "base64url";
+
+declare type ISelectResult = Array<Node> | Node | boolean | number | string;
+
+interface XmlNamespace {
+    prefix: string | null;
+    namespace: string | null;
+}
+
+declare namespace XmlJs {
 
     export class Collection<I> {
         protected items: Array<I>;
@@ -228,4 +261,8 @@ declare namespace xmljs {
         GetFirstChild(localName: string, namespace?: string): Element | null;
     }
 
+}
+
+declare module "xmljs" {
+    export = XmlJs
 }
