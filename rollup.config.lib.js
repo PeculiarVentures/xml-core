543544e44cba3a5448b9ff859abe90abeaa1a8bf
microshine <microshine@mail.ru>
2017-01-15 18:25:45 +0300

    Update scripts

--- rollup.config.lib.js
+++ rollup.config.lib.js
@@ -1,6 +1,4 @@
 import typescript from "rollup-plugin-typescript";
-import babel from "rollup-plugin-babel";
-import babelrc from "babelrc-rollup";
 
 let pkg = require("./package.json");
 let external = Object.keys(pkg.dependencies);
@@ -11,14 +9,12 @@ export default {
     entry: "src/index.ts",
     plugins: [
         typescript({ typescript: require("typescript") }),
-        babel(babelrc()),
     ],
     external: external,
     targets: [
         {
-            dest: pkg.main,
-            format: "umd",
-            moduleName: "XmlCore",
+            dest: pkg.module,
+            format: "es",
             sourceMap
         }
     ]
